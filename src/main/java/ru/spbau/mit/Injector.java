81cd1fd070ed86b6371fe02a8682441115cc2f77
ComradePetr <petr1996@bk.ru>
2015-12-02 16:55:27 +0300

    First commit

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,5 +1,8 @@
 package ru.spbau.mit;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -9,7 +12,55 @@ public class Injector {
      * Create and initialize object of `rootClassName` class using classes from
      * `implementationClassNames` for concrete dependencies.
      */
+    static Class<?>[] implementationClasses = null;
+    static HashMap<Class<?>, Object> saved = new HashMap<Class<?>,Object>();
+
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        implementationClasses = new Class<?>[implementationClassNames.size()];
+        saved.clear();
+        int i = 0;
+        for (String implementationClassName : implementationClassNames) {
+            implementationClasses[i++] = Class.forName(implementationClassName);
+        }
+
+        Class<?> rootClass = Class.forName(rootClassName);
+        return initialize(rootClass);
+    }
+
+    private static Object initialize(Class<?> rootClass) throws Exception {
+        if (saved.containsKey(rootClass)) {
+            Object createdObject = saved.get(rootClass);
+            if (createdObject == null) {
+                throw new InjectionCycleException();
+            } else {
+                return createdObject;
+            }
+        }
+        saved.put(rootClass, null);
+
+        Constructor<?>[] constructors = rootClass.getDeclaredConstructors();
+        if (constructors.length != 1) {
+            throw new IllegalStateException();
+        }
+
+        Constructor<?> constructor = constructors[0];
+        ArrayList<Object> parametersList = new ArrayList<>();
+        for (Class<?> parameterType : constructor.getParameterTypes()) {
+            ArrayList<Class<?>> candidates = new ArrayList<>();
+            for (Class<?> curClass : implementationClasses) {
+                if (parameterType.isAssignableFrom(curClass)) {
+                    candidates.add(curClass);
+                }
+            }
+            if (candidates.isEmpty()) {
+                throw new ImplementationNotFoundException();
+            }
+            if (candidates.size() > 1) {
+                throw new AmbiguousImplementationException();
+            }
+            parametersList.add(initialize(candidates.get(0)));
+        }
+
+        return constructor.newInstance(parametersList.toArray());
     }
 }
